136e4e3b31d3a0327cc21ce84f09a0a73afb254a
Spencer Dixon <spencercdixon@gmail.com>
2014-09-14 11:46:33 -0400

    Can now see whether or not word is spelled correctly

--- receipt.rb
+++ receipt.rb
@@ -1,4 +1,5 @@
 require 'tesseract'
+require 'ffi/aspell'
 require 'pry'
 
 
@@ -23,5 +24,31 @@ text = codepen.transcribe
 # confidence = codepen.confidence(path)
 
 sentences = text.split("\n")
-sentences[9]
-binding.pry
+split_sentence = sentences[9].split(' ')
+
+
+split_sentence.each do |w|
+  # Try and fix an individual sentence
+  speller  = FFI::Aspell.speller_new(FFI::Aspell.config_new)
+  word     = w
+  valid = FFI::Aspell.speller_check(speller, w, w.length)
+  if valid
+    puts word
+  else
+    puts "word is wrong"
+  end
+
+
+#   list     = FFI::Aspell.speller_suggest(speller, word, word.length)
+#   elements = FFI::Aspell.word_list_elements(list)
+#   words    = []
+#
+#
+#   while word = FFI::Aspell.string_enumeration_next(elements)
+#     words << word
+#     binding.pry
+#   end
+#
+#   FFI::Aspell.string_enumeration_delete(elements)
+#   FFI::Aspell.speller_delete(speller)
+end
