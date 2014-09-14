1ea5b3688f00f343ef51e8ccab5e963571ce578d
Spencer Dixon <spencercdixon@gmail.com>
2014-09-14 19:55:35 -0400

    Have working number totals, need to fix optimal spell checker section

--- receipt.rb
+++ receipt.rb
@@ -2,6 +2,11 @@ require 'tesseract'
 require 'ffi/aspell'
 require 'pry'
 
+class String
+  def cost_to_f
+    self.delete('$,').to_f
+  end
+end
 
 class Receipt
   attr_reader :path
@@ -17,14 +22,14 @@ class Receipt
 
 end
 
-
-path = '~/Desktop/receipt1.png'
+path = '~/Desktop/trader2.jpg'
 codepen = Receipt.new(path)
 text = codepen.transcribe
+
 # confidence = codepen.confidence(path)
 
 sentences = text.split("\n")
-split_sentence = sentences[9].split(' ')
+split_sentence = sentences[-4].split(' ')
 
 new_sentence = []
 split_sentence.each do |w|
@@ -34,6 +39,8 @@ split_sentence.each do |w|
   valid = FFI::Aspell.speller_check(speller, w, w.length)
   if valid
     new_sentence << word
+  elsif word.cost_to_f > 0
+    new_sentence << word
   else
 
     list     = FFI::Aspell.speller_suggest(speller, word, word.length)
@@ -42,13 +49,15 @@ split_sentence.each do |w|
 
     while word = FFI::Aspell.string_enumeration_next(elements)
       words << word
-      new_sentence << words[0]
-      break
+      if word == "SUBTOTAL"
+        new_sentence << words[1]
+      end
     end
 
     FFI::Aspell.string_enumeration_delete(elements)
     FFI::Aspell.speller_delete(speller)
   end
 end
+
 
 puts new_sentence
